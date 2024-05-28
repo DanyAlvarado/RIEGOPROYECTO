ca59572fd8dd5582b60da369f867691e1fea87c6
Diego <diegocosillo@gmail.com>
2024-05-28 17:04:03 -0600

    Codigo para funcionalidad de relay

--- PROYECTO_ARQUI/PROYECTO_ARQUI.ino
+++ PROYECTO_ARQUI/PROYECTO_ARQUI.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include <LCD_I2C.h>
-
+#include <DHT.h>
 #define clk 7
 #define dt 6
 #define btn 4
@@ -14,6 +14,20 @@ String menu_med [] = {"Luz", "Lluvia", "Temperatura", "Humedad", "Regresar"};
 int state_clk_old;
 int state_btn_old;
 int count = 0;
+const int pinRelaySuelo = 11;
+const int pinRelayLuz = 10;
+bool mostrarMediciones = false;
+const int pinDHT11 = 3;
+
+/*int humedad;
+int luz;
+int temp;
+int lluvia;
+*/
+
+#define DHTTYPE DHT11
+DHT dht(pinDHT11, DHTTYPE);
+
 
 
 void setup(){
@@ -22,9 +36,13 @@ void setup(){
   pinMode(clk, INPUT);
   pinMode(dt, INPUT);
   pinMode(btn, INPUT_PULLUP);
+  pinMode(pinRelaySuelo, OUTPUT);
+  pinMode(pinRelayLuz, OUTPUT);
+
   state_clk_old = digitalRead(clk);
   state_btn_old = digitalRead(btn);
   mostrar_menu_principal();
+  dht.begin();
 }
 
 void loop(){
@@ -33,10 +51,14 @@ void loop(){
 
   encoder();
   
-
+  
   if (state_btn_old == HIGH && state_btn == LOW) {
     correr_opcion();
   }
+
+  if(mostrarMediciones){
+    mostrar_mediciones();
+  }  
 
   state_btn_old = state_btn;  // Actualizar estado del botón
 
@@ -45,7 +67,10 @@ void loop(){
 
 void correr_opcion() {
   if (count == 0) {
-    mostrar_mediciones();
+    mostrarMediciones = true;
+  }else{
+    mostrarMediciones = false;
+    mostrar_menu_principal();
   }
 }
 
@@ -80,9 +105,9 @@ void mostrar_menu_principal() {
 void mostrar_mediciones() {
   int humedad = analogRead(A1); 
   int luz = analogRead(A3);
-  int temp = analogRead(A0);
+  float temp = dht.readTemperature();
   int lluvia = analogRead(A2);
-  temp = (5.0 * temp * 100.0) / 1024.0;
+  //temp = (5.0 * temp * 100.0) / 1024.0;
   lcd_i2c.clear();
   lcd_i2c.setCursor(0, 0);
   lcd_i2c.print("HUMEDAD TIERRA: ");
@@ -96,8 +121,30 @@ void mostrar_mediciones() {
   lcd_i2c.setCursor(0, 3);
   lcd_i2c.print("LLUVIA: ");
   lcd_i2c.print(lluvia);
+  //El riego se activa cuando la humedad baja de 800
+  if(humedad < 800){
+    digitalWrite(pinRelaySuelo,HIGH);
+  }else{
+    digitalWrite(pinRelaySuelo,LOW);
+  }
+  //El riego se evita cuando la temperatura es mayor a 30
+  if(temp > 30.0){
+    digitalWrite(pinRelaySuelo, HIGH);
+  }
 
-  delay(1000);  
+  if(luz > 500){
+    digitalWrite(pinRelayLuz, HIGH);
+  } else {
+    digitalWrite(pinRelayLuz, LOW);
+  }
+
   
-}delay(1000); // 1 segundo (ajusta este valor según sea necesario)
+  delay(1000);  
+
+
+  
 }
+void controlarRele(){
+  
+}
+
